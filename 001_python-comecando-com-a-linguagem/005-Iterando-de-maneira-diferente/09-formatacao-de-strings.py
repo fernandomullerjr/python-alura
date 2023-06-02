7a07877f556ed799a5555d980b76d5f718eeb046
Fernando Muller Junior <fernandomj90@gmail.com>
2023-06-01 23:41:16 -0300

    Modulo 5 - aula 10 Adaptando o Padrão Americano.

--- 001_python-comecando-com-a-linguagem/005-Iterando-de-maneira-diferente/09-formatacao-de-strings.py
+++ 001_python-comecando-com-a-linguagem/005-Iterando-de-maneira-diferente/09-formatacao-de-strings.py
@@ -4,7 +4,6 @@ print("Tentativa {} de {}".format(1, 3))
 
 
 ## Formatação de floats
-
 print("R$ {:.2f}".format(1.59))
 # R$ 1.59
 
@@ -14,7 +13,6 @@ print("R$ {:07.2f}".format(1.5))
 
 
 ## INTEIROS
-
 # Conseguimos formatar números inteiros também, não só números flutuantes. Para números inteiros, passamos a letra d:
 print("R$ {:07d}".format(4))
 # R$ 0000004
---
+++ 001_python-comecando-com-a-linguagem/005-Iterando-de-maneira-diferente/10.py
@@ -0,0 +1,8 @@
+
+print("Ola Sr.{-1} {1}".format("Cordeiro","Leonardo"))
+
+print("Ola Sr.{2} {1}".format("Cordeiro","Leonardo"))
+
+print("Ola Sr.{1} {0}".format("Cordeiro","Leonardo"))
+
+print("Ola Sr.{0} {1}".format("Cordeiro","Leonardo"))

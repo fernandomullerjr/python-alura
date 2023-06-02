13e355b410d0c7da0d56d749e146d3b1f1812a41
Fernando Muller Junior <fernandomj90@gmail.com>
2023-06-02 00:01:19 -0300

    Modulo 5 - aula 12 Interpolação - Python 2 vs Python 3.

---
+++ 001_python-comecando-com-a-linguagem/005-Iterando-de-maneira-diferente/f-string.py
@@ -0,0 +1,4 @@
+# A partir da versão 3.6 do Python, foi adicionado um novo recurso para realizar a interpolação de strings. Esse recurso é chamado de f-strings ou formatted string literals.
+# Esse recurso funciona da seguinte forma. Vamos imaginar que temos uma variável nome:
+nome = 'Matheus'
+print(f'Meu nome é {nome}')

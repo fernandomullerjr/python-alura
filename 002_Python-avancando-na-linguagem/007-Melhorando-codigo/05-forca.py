87d0db417da8886b5fd49976146e80e54122bc9a
Fernando Müller Junior <fernandomj90@gmail.com>
2024-10-09 00:16:10 -0300

    Módulo 7 - 05 Melhorando a apresentação da forca.

--- 002_Python-avancando-na-linguagem/007-Melhorando-codigo/05-forca.py
+++ 002_Python-avancando-na-linguagem/007-Melhorando-codigo/05-forca.py
@@ -25,7 +25,6 @@ def jogar():
     else:
         imprime_mensagem_perdedor(palavra_secreta)
     print("Fim do jogo")
-
 def desenha_forca(erros):
     print("  _______     ")
     print(" |/      |    ")
@@ -38,39 +37,39 @@ def desenha_forca(erros):
 
     if(erros == 2):
         print(" |      (_)   ")
-        print(" |      \     ")
+        print(" |      \\     ")
         print(" |            ")
         print(" |            ")
 
     if(erros == 3):
         print(" |      (_)   ")
-        print(" |      \|    ")
+        print(" |      \\|    ")
         print(" |            ")
         print(" |            ")
 
     if(erros == 4):
         print(" |      (_)   ")
-        print(" |      \|/   ")
+        print(" |      \\|/   ")
         print(" |            ")
         print(" |            ")
 
     if(erros == 5):
         print(" |      (_)   ")
-        print(" |      \|/   ")
+        print(" |      \\|/   ")
         print(" |       |    ")
         print(" |            ")
 
     if(erros == 6):
         print(" |      (_)   ")
-        print(" |      \|/   ")
+        print(" |      \\|/   ")
         print(" |       |    ")
         print(" |      /     ")
 
     if (erros == 7):
         print(" |      (_)   ")
-        print(" |      \|/   ")
+        print(" |      \\|/   ")
         print(" |       |    ")
-        print(" |      / \   ")
+        print(" |      / \\   ")
 
     print(" |            ")
     print("_|___         ")
@@ -124,21 +123,21 @@ def imprime_mensagem_perdedor(palavra_secreta):
     print("Puxa, você foi enforcado!")
     print("A palavra era {}".format(palavra_secreta))
     print("    _______________         ")
-    print("   /               \       ")
-    print("  /                 \      ")
-    print("//                   \/\  ")
-    print("\|   XXXX     XXXX   | /   ")
+    print("   /               \\       ")
+    print("  /                 \\      ")
+    print("//                   \\/\\  ")
+    print("\\|   XXXX     XXXX   | /   ")
     print(" |   XXXX     XXXX   |/     ")
     print(" |   XXX       XXX   |      ")
     print(" |                   |      ")
-    print(" \__      XXX      __/     ")
-    print("   |\     XXX     /|       ")
+    print(" \\__      XXX      __/     ")
+    print("   |\\     XXX     /|       ")
     print("   | |           | |        ")
     print("   | I I I I I I I |        ")
     print("   |  I I I I I I  |        ")
-    print("   \_             _/       ")
-    print("     \_         _/         ")
-    print("       \_______/           ")
+    print("   \\_             _/       ")
+    print("     \\_         _/         ")
+    print("       \\_______/           ")
 
 
 if(__name__ == "__main__"):
